2f7cc4967e01eda5236e9518a96e1c4d4b8f64cd
SunTiebing <1045060705@qq.com>
2023-06-09 21:01:27 +0800

    optimize migrations

--- pallets/kitties/src/migrations/v1.rs
+++ pallets/kitties/src/migrations/v1.rs
@@ -1,25 +1,24 @@
 use crate::{Config, Kitties, Kitty, KittyId, Pallet};
+use frame_support::{migration::storage_key_iter, Blake2_128Concat};
 use frame_support::{
-	migration::storage_key_iter, pallet_prelude::*, storage::StoragePrefixedMap,
-	traits::GetStorageVersion, weights::Weight, Blake2_128Concat,
+	pallet_prelude::*, storage::StoragePrefixedMap, traits::GetStorageVersion, weights::Weight,
 };
 
 #[derive(
-	Encode, Decode, Default, Clone, Copy, PartialEq, Eq, RuntimeDebug, TypeInfo, MaxEncodedLen,
+Encode, Decode, Default, Clone, Copy, PartialEq, Eq, RuntimeDebug, TypeInfo, MaxEncodedLen,
 )]
 pub struct V0Kitty(pub [u8; 16]);
 
-/// version 0 to version 2
 pub fn migrate<T: Config>() -> Weight {
 	let on_chain_version = Pallet::<T>::on_chain_storage_version();
 	let current_version = Pallet::<T>::current_storage_version();
 
-	// only works for version 0 to 2
+	// only works for version 0 to 1
 	if on_chain_version != 0 {
-		return Weight::zero()
+		return Weight::zero();
 	}
-	if current_version != 2 {
-		return Weight::zero()
+	if current_version != 1 {
+		return Weight::zero();
 	}
 
 	let module = Kitties::<T>::module_prefix();
@@ -27,12 +26,9 @@ pub fn migrate<T: Config>() -> Weight {
 
 	// using drain() method to abandon old data
 	for (index, kitty) in
-		storage_key_iter::<KittyId, V0Kitty, Blake2_128Concat>(module, item).drain()
+	storage_key_iter::<KittyId, V0Kitty, Blake2_128Concat>(module, item).drain()
 	{
-		// using the first eight dna to be default name
-		let mut name: [u8; 8] = [0; 8];
-		name.clone_from_slice(&kitty.0[..8]);
-		let new_kitty = Kitty { dna: kitty.0, name };
+		let new_kitty = Kitty { dna: kitty.0, name: *b"abcd" };
 		Kitties::<T>::insert(index, &new_kitty);
 	}
 
--- pallets/kitties/src/migrations/v2.rs
+++ pallets/kitties/src/migrations/v2.rs
@@ -7,18 +7,21 @@ use frame_support::{
 #[derive(
 	Encode, Decode, Default, Clone, Copy, PartialEq, Eq, RuntimeDebug, TypeInfo, MaxEncodedLen,
 )]
+pub struct V0Kitty(pub [u8; 16]);
+#[derive(
+	Encode, Decode, Default, Clone, Copy, PartialEq, Eq, RuntimeDebug, TypeInfo, MaxEncodedLen,
+)]
 pub struct V1Kitty {
 	pub dna: [u8; 16],
 	pub name: [u8; 4],
 }
 
-/// version 1 to version 2
 pub fn migrate<T: Config>() -> Weight {
 	let on_chain_version = Pallet::<T>::on_chain_storage_version();
 	let current_version = Pallet::<T>::current_storage_version();
 
-	// only works for version 1 to 2
-	if on_chain_version != 1 {
+	// works for version 1 to 2 or version 0 to 2
+	if on_chain_version != 1 || on_chain_version != 0 {
 		return Weight::zero()
 	}
 	if current_version != 2 {
@@ -28,15 +31,31 @@ pub fn migrate<T: Config>() -> Weight {
 	let module = Kitties::<T>::module_prefix();
 	let item = Kitties::<T>::storage_prefix();
 
-	for (index, kitty) in
-		storage_key_iter::<KittyId, V1Kitty, Blake2_128Concat>(module, item).drain()
-	{
-		// new name rule
-		let mut name = [0u8; 8];
-		name[..4].copy_from_slice(&kitty.name);
-		name[4..].copy_from_slice(&kitty.name);
-		let new_kitty = Kitty { dna: kitty.dna, name };
-		Kitties::<T>::insert(index, &new_kitty);
+	// version 0 to version 2
+	if on_chain_version == 0 {
+		for (index, kitty) in
+			storage_key_iter::<KittyId, V0Kitty, Blake2_128Concat>(module, item).drain()
+		{
+			// using the first eight dna to be default name
+			let mut name: [u8; 8] = [0; 8];
+			name.clone_from_slice(&kitty.0[..8]);
+			let new_kitty = Kitty { dna: kitty.0, name };
+			Kitties::<T>::insert(index, &new_kitty);
+		}
+	}
+
+	// version 1 to version 2
+	if on_chain_version == 1 {
+		for (index, kitty) in
+			storage_key_iter::<KittyId, V1Kitty, Blake2_128Concat>(module, item).drain()
+		{
+			// new name rule
+			let mut name = [0u8; 8];
+			name[..4].copy_from_slice(&kitty.name);
+			name[4..].copy_from_slice(&kitty.name);
+			let new_kitty = Kitty { dna: kitty.dna, name };
+			Kitties::<T>::insert(index, &new_kitty);
+		}
 	}
 
 	Weight::zero()
